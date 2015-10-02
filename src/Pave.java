9def56bd2003dcf69a03f5b4c7ee2df42fa344c9
Alexandre Hubert <louis-alexandre.hubert@isep.fr>
2015-10-02 20:53:58 +0200

    Update Pave.java

--- src/Pave.java
+++ src/Pave.java
@@ -1,9 +1,22 @@
 import java.util.Scanner;
 
 public class Pave {
-	
-	Scanner scan = new Scanner(System.in);
-	
-	//
+	public static void main(String [] args){
+		double volume=0,longueur=0,largeur=0,hauteur=0;
+		Scanner scan = new Scanner(System.in);
+		do{
+			System.out.println("Veuillez saisir la longueur :");
+			longueur = scan.nextDouble();
+		}while(longueur<0);
+		do{
+			System.out.println("Veuillez saisir la largeur :");
+			largeur = scan.nextDouble();
+		}while(largeur<0);
+		do{
+			System.out.println("Veuillez saisir la hauteur :");
+			hauteur = scan.nextDouble();
+		}while(hauteur<0);
+		volume=longueur*largeur*hauteur;
+		System.out.println("Volume du pavé : " + volume);
 	}
 }
